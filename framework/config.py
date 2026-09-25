5929968fd727707f01b2fc8d3c13987041cdd3b5
Yatindra-Malpani <yatin.narayan.2005@gmail.com>
2026-09-26 00:36:17 +0530

    Refactor model configuration in config.py
    Updated model configuration to include multiple models and set NUM_RUNS.

--- framework/config.py
+++ framework/config.py
@@ -7,15 +7,16 @@ HF_HOME = r"E:\benchmarking\hf_home"
 os.environ["HF_HOME"] = HF_HOME
 
 # Model Configuration
-'''MODEL_NAME = "Qwen/Qwen2.5-1.5B-Instruct"'''
+# Model Configuration
+MODELS = [
+    "Qwen/Qwen2.5-0.5B-Instruct",
+    "Qwen/Qwen2.5-1.5B-Instruct",
+    "microsoft/Phi-3.5-mini-instruct",
+    "HuggingFaceTB/SmolLM2-1.7B-Instruct",
+    "ministral/Ministral-3b-instruct",
+]
 
-'''MODEL_NAME = "Qwen/Qwen2.5-0.5B-Instruct" '''
-
-'''MODEL_NAME = "microsoft/Phi-3.5-mini-instruct"'''
-
-'''MODEL_NAME = "HuggingFaceTB/SmolLM2-1.7B-Instruct"'''
-
-MODEL_NAME = "ministral/Ministral-3b-instruct"
+NUM_RUNS = 3
 
 # Quantization Configuration
 LOAD_IN_4BIT = True
