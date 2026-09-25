e8f9f1a301d757cd0dacaaeb1f06e1d242938381
Yatindra-Malpani <yatin.narayan.2005@gmail.com>
2026-09-26 00:39:18 +0530

    Refactor load_model to accept model_name parameter

--- framework/loader.py
+++ framework/loader.py
@@ -4,25 +4,28 @@ from transformers import (
     AutoModelForCausalLM,
     BitsAndBytesConfig,
 )
+
 from src.framework.config import (
-    MODEL_NAME,
     LOAD_IN_4BIT,
     QUANT_TYPE,
     COMPUTE_DTYPE,
     DOUBLE_QUANT,
     DEVICE_MAP,
 )
+
 def create_bnb_config():
     """
     Create the BitsAndBytes quantization configuration.
     """
+
     return BitsAndBytesConfig(
         load_in_4bit=LOAD_IN_4BIT,
         bnb_4bit_quant_type=QUANT_TYPE,
         bnb_4bit_compute_dtype=COMPUTE_DTYPE,
         bnb_4bit_use_double_quant=DOUBLE_QUANT,
     )
-def load_model():
+
+def load_model(model_name):
     """
     Load tokenizer and model.
 
@@ -33,13 +36,14 @@ def load_model():
     """
     start_time = time.perf_counter()
 
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     model = AutoModelForCausalLM.from_pretrained(
-        MODEL_NAME,
+        model_name,
         quantization_config=create_bnb_config(),
         device_map=DEVICE_MAP,
     )
+
     load_time = time.perf_counter() - start_time
 
     return tokenizer, model, load_time
